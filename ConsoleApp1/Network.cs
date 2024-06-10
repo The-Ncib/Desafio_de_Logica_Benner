f34c9c26730c26df814dfc65cd3bece9d2e457a7
ncibn <ncibn@DESKTOP-24MTRP5>
2024-06-10 03:32:08 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Network.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Network
+    {
+        private int[] pai;
+
+        public Network(int tamanho)
+        {
+            if (tamanho <= 1)
+            {
+                throw new Exception("O valor informado precisa ser válido!");
+            }
+
+            pai = new int[tamanho + 1];
+            for (int i = 1; i <= tamanho; i++)
+            {
+                pai[i] = i; 
+            }
+        }
+
+        private int Achar(int x)
+        {
+            if (pai[x] != x) 
+            {
+                pai[x] = Achar(pai[x]); 
+            }
+            return pai[x];
+        }
+
+        public void Connect(int num1, int num2)
+        {
+            if (num1 <= 0 || num2 <= 0 || num1 >= pai.Length || num2 >= pai.Length)
+            {
+                throw new Exception("Número inválido.");
+            }
+
+            int raiz1 = Achar(num1);
+            int raiz2 = Achar(num2);
+
+            if (raiz1 != raiz2)
+            {
+                pai[raiz2] = raiz1; // Torna o raiz2 filho do raiz1, assim sendo, no query, os dois terão o mesmo valor, logo, mesmo conjunto. 
+            }
+        }
+
+        public bool Query(int num1, int num2)
+        {
+            if (num1 <= 0 || num2 <= 0 || num1 >= pai.Length || num2 >= pai.Length)
+            {
+                throw new Exception("Número inválido.");
+            }
+
+            return Achar(num1) == Achar(num2); //
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Olá! Tudo bem? Informe o tamanho do set de dados: "); 
+
+            
+            int numSet = Convert.ToInt32(Console.ReadLine());
+
+            Network rede = null; 
+
+            try
+            {
+                rede = new Network(numSet);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Pressione qualquer tecla para sair");
+                Console.ReadKey();
+                Environment.Exit(1); 
+
+              
+            }
+
+            bool saida = false;
+
+            while (!saida)
+            {
+
+                Console.WriteLine("1 - Informar duas novas conexões\n" +
+                "2 - Verificar se dois números estão conectados\n" +
+                "3 - Fechar programa");
+
+                int esc = Convert.ToInt32(Console.ReadLine());
+
+                switch (esc)
+                {
+                    case 1:
+
+                        try
+                        {
+                            Console.Clear();
+                            Console.Write("Informe o primeiro número a ser conectado: ");
+                            int num1 = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Informe o segundo número a ser conectado: ");
+                            int num2 = Convert.ToInt32(Console.ReadLine());
+                            rede.Connect(num1, num2);
+                        }
+                        catch (Exception e )
+                        {
+                            Console.WriteLine(e.Message);
+                            Console.WriteLine("Pressione qualquer tecla para sair");
+                            Console.ReadKey();
+                            Environment.Exit(1);
+                        }
+                        break;
+
+                    case 2:
+
+                        try
+                        {
+                            Console.Clear();
+                            Console.Write("Informe o primeiro número a ser verificado: ");
+                            int num3 = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Informe o segundo número a ser verificado: ");
+                            int num4 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(rede.Query(num3, num4));
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                            Console.WriteLine("Pressione qualquer tecla para sair");
+                            Console.ReadKey();
+                            Environment.Exit(1);
+                        }
+                        break;
+
+                    case 3:
+                        saida = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Favor informar uma opção válida!");
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
